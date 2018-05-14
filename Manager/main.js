8f6665b189398b3b03c35833804893af5c373742
Maldrek <scott.salem5@gmail.com>
2018-05-14 18:45:38 -0500

    tried updating some HTML and added a form for creating a ticket

--- Manager/main.js
+++ Manager/main.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
+    var collapse = document.querySelectorAll('.collapsible');
     var options = {
         accordion: true,
         induration: 1000
     };
-    var instances = M.Collapsible.init(elems, options);
+    var instances = M.Collapsible.init(collapse, options);
+
+    var select = document.querySelectorAll('select');
+    var options = {
+        classes: "",
+        dropdownOptions: {}
+    }
+    var instances = M.FormSelect.init(select, options);
   });
+
+
